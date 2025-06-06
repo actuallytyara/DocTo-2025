38ff8593011547bcb75babfdd7fd0ae1f365ea58
actuallytyara <ghatax123@gmail.com>
2025-06-06 10:27:42 +0700

    Add files via upload

---
+++ Tentang.php
@@ -0,0 +1,40 @@
+<?php
+header("refresh:5;url=Tentang.php");
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DocTo - tentang</title>
+    <link rel="stylesheet" href="assets/css/tentang.css">
+</head>
+
+<body>              
+    <div class="main-container">
+        <div class="image-section">
+            <img src="assets/images/Dokter.jpg" alt="Dokter">
+        </div>
+        <div class="content-section">
+            <div class="about-card"> 
+                <a class="logo"> <img src="assets/images/docto.png" style="width: 60px; height: 70px; margin-bottom: 5px;"></a>
+                <h2>Tentang Kami</h2>
+                <p>Kami menyediakan Layanan Kesehatan yang unggul dan sangat bermanfaat untuk kebutuhan kesehatan anda terutama untuk kebutuhan keluarga, Pada web ini kami tersedia layanan chat dengan dokter langsung, artikel maupun penjualan online obat secara langsung, Perusahaan kami berada di jepang dan didirikan pada Tahun 2014, Kami sangat menghormati anda Kami disini melayani anda 24 jam, Anda sehat Kami senang.</p>
+                <a href="index.php" class="kembali-btn">Kembali</a>
+            </div>
+
+            <div class="contact-card">
+                <h3>Kontak</h3>
+                <ul class="contact-info">
+                    <li>Email: info@docto.com</li>
+                    <li>Telepon: (021) 1234 5678</li>
+                </ul>
+                <p class="copyright">2024 © DoctoAja</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ articles.php
@@ -0,0 +1,22 @@
+<?php
+$articles = [
+    [
+        'title' => 'Pentingnya Pola Makan Sehat',
+        'description' => 'Pelajari bagaimana pola makan sehat dapat meningkatkan kesehatan Anda.',
+        'link' => 'https://doktersehat.com/gaya-hidup/gizi-dan-nutrisi/pola-makan-sehat/',
+        'thumbnail' => 'assets/images/Salad.jpeg' 
+    ],
+    [
+        'title' => 'Tips Olahraga di Rumah',
+        'description' => 'Cara tetap bugar tanpa perlu ke gym dengan lebih efisien di rumah.',
+        'link' => 'https://www.liputan6.com/feeds/read/5796800/tips-olahraga-di-rumah-panduan-lengkap-untuk-tetap-bugar',
+        'thumbnail' => 'assets/images/Yoga.jpeg' 
+    ],
+    [
+        'title' => 'Mengelola Stres dengan Efektif',
+        'description' => 'Teknik relaksasi untuk menjaga kesehatan mental.',
+        'link' => 'https://www.kompasiana.com/juardimanputragea3244/67426c01c925c4240f2ed6a5/stres-no-more-cara-mengelola-stres-dengan-efektif',
+        'thumbnail' => 'assets/images/download.jpeg'
+    ]
+];
+?>
